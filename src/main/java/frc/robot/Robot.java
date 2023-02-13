6c79376ace4e8644b4cd5a43275ee9cad6bbc2a7
LongmetalAdmin <115046046+LongmetalAdmin@users.noreply.github.com>
2023-02-12 23:55:41 -0500

    auto multiple

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,143 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.lang.ref.Cleaner;
+
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.wpilibj.AnalogInput;
+// import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
+
+  // private final DigitalOutput trig = new DigitalOutput(9);
+  // private final AnalogInput echo = new AnalogInput(0);
+  
+  // distance in inches the robot wants to stay from an object
+  private static final double kHoldDistance = 12.0;
+
+  // factor to convert sensor values to a distance in inches
+  private static final double kValueToInches = 0.125;
+
+  // proportional speed constant
+  private static final double kP = 0.05;
+
+  // private static final int kUltrasonicPort = 0;
+
+  // median filter to discard outliers; filters over 10 sampleshttps://open.spotify.com/track/6p5CmR50Qd1e3Ouma2e8yx?si=36230758b6d94a47
+  private final MedianFilter m_filter = new MedianFilter(10);
+  
+  // private final AnalogInput m_ultrasonic = new AnalogInput(kUltrasonicPort);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    //  Shuffleboard.getTab("Example tab").add(m_ultrasonic);
+    // Shuffleboard.getTab("Example tab").add(echo);    
+
+    m_robotContainer = new RobotContainer();  
+    // m_robotContainer.trig.set(true);
+    // System.out.println("trig: " + m_robotContainer.trig.get());
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.   
+
+    // double currentDistance = m_filter.calculate(m_ultrasonic.getValue()) * kValueToInches;
+    // convert distance error to a motor speed
+    // double currentSpeed = (kHoldDistance - currentDistance) * kP;
+
+    // System.out.println(m_ultrasonic.get());
+    // for(var x = 0 ; x!=1000000000 ; x++){
+      // if(x == 100000000){
+        //m_robotContainer.trig.pulse(100);
+        // System.out.println("echo: " + m_robotContainer.echo.getValue());
+        // System.out.println("trig: " + trig.isPulsing());
+        //trig.set(false);
+        // x=0;
+      // }
+      // System.out.println("trig: " + trig.isPulsing());
+    // }
+
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    // Get selected routine from the SmartDashboard
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running which will
+    // use the default command which is ArcadeDrive. If you want the autonomous
+    // to continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,133 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.commands.auto.AutonomousCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.AutonomousDistance;
+import frc.robot.commands.AutonomousTime;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.OnBoardIO;
+import frc.robot.subsystems.OnBoardIO.ChannelMode;
+import frc.robot.GameController;
+import frc.robot.Constants;
+import frc.robot.Constants.OiConstants;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final OnBoardIO m_onboardIO = new OnBoardIO(ChannelMode.INPUT, ChannelMode.INPUT);
+
+  // Assumes a gamepad plugged into channnel 0
+  private final Joystick m_controller = new Joystick(9);
+
+  // Create SmartDashboard chooser for autonomous routines
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  // public final DigitalOutput trig = new DigitalOutput(9);
+  public final AnalogInput echo = new AnalogInput(0);
+
+  private final GameController driverController = new GameController(OiConstants.DRIVER_CONTROLLER_PORT);
+  SendableChooser<String>      autoChooser      = new SendableChooser<>();
+  
+  // private static final DigitalOutput mb1013o = new DigitalOutput(9);
+  // private static final AnalogInput mb1013i = new AnalogInput(0);
+
+  // new PrintCommand("here");
+  // Ultrasonic.setAutomaticMode(true);
+  // Shuffleboard.getTab("Example tab").add(ultrasonic);
+  // // return new PrintCommand(ultrasonic.isEnabled());
+  // double mm = ultrasonic.getRangeMM();
+  // new PrintCommand("mm: " + String.valueOf(mm));
+
+  // NOTE: The I/O pin functionality of the 5 exposed I/O pins depends on the hardware "overlay"
+  // that is specified when launching the wpilib-ws server on the Romi raspberry pi.
+  // By default, the following are available (listed in order from inside of the board to outside):
+  // - DIO 8 (mapped to Arduino pin 11, closest to the inside of the board)
+  // - Analog In 0 (mapped to Analog Channel 6 / Arduino Pin 4)
+  // - Analog In 1 (mapped to Analog Channel 2 / Arduino Pin 20)
+  // - PWM 2 (mapped to Arduino Pin 21)
+  // - PWM 3 (mapped to Arduino Pin 22)
+  //
+  // Your subsystem configuration should take the overlays into account
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings(); 
+    // Ultrasonic.setAutomaticMode(true); 
+    // Shuffleboard.getTab("Example tab").add(trig);
+    // Shuffleboard.getTab("Example tab").add(echo); 
+
+    SmartDashboard.putNumber("gyrox", m_drivetrain.getGyroAngleX());
+    SmartDashboard.putNumber("gyrox", m_drivetrain.getGyroAngleY());
+    SmartDashboard.putNumber("gyrox", m_drivetrain.getGyroAngleZ());
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Default command is arcade drive. This will run unless another command
+    // is scheduled over it.
+    m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
+
+    // Example of how to use the onboard IO
+    Button onboardButtonA = new Button(m_onboardIO::getButtonAPressed);
+    onboardButtonA
+        .whenActive(ping())
+        .whenInactive(new PrintCommand("Button A Released"));
+
+    // Setup SmartDashboard options
+    m_chooser.setDefaultOption("Auto Routine Distance", new AutonomousDistance(m_drivetrain));
+    m_chooser.addOption("Auto Routine Time", new AutonomousTime(m_drivetrain));
+    SmartDashboard.putData(m_chooser);
+
+  }
+
+  private Command ping(){ 
+    return new PrintCommand("Button A Pressed");
+  }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return new AutonomousCommand(m_drivetrain, autoChooser);
+    // return m_chooser.getSelected();
+  }
+
+  /**
+   * Use this to pass the teleop command to the main {@link Robot} class.
+   *
+   * @return the command to run in teleop
+   */
+  public Command getArcadeDriveCommand() {
+    return new ArcadeDrive(
+        m_drivetrain, () -> -m_controller.getRawAxis(1), () -> m_controller.getRawAxis(2));
+
+  }
+}
